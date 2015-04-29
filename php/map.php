c921ca00b653f7a536ac5528a4fbf4ef9f0269ff
MAJA-Lin <typhoon31815@gmail.com>
2015-04-30 02:03:18 +0800

    Basic Map funcion to show bookstores on google map

--- php/map.php
+++ php/map.php
@@ -1,5 +1,20 @@
-<!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
-	</head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Map Test</title>
+</head>
+<body>
+	<?php
+	$bookstore_address = array('台中市西區公益路68號',
+								'臺中市北區三民路三段161號');
+	?>
+	<form method="post" action="showmap.php">
+		<input type="radio" name="bookstore" value="<?php echo $bookstore_address[0]; ?>" checked>
+		勤美誠品<br>
+		<input type="radio" name="bookstore" value="<?php echo $bookstore_address[1]; ?>">
+		中友百貨誠品<br>
+		<input type="submit" value="檢視地圖">
+	</form>
+</body>
+</html>
---
+++ php/showmap.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Show Map</title>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false">
+	</script>
+	<script type="text/javascript">
+		var geocoder;
+		var map;
+
+		function initialize()
+		{
+			geocoder = new google.maps.Geocoder();
+			var latlng = new google.maps.LatLng(0,0);
+			var myOptions = {zoom:10,center:latlng, mapTypeId:google.maps.MapTypeId.ROADMAP};
+			map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+			codeAddress();
+		}
+
+		function codeAddress();
+		{
+			var address = document.getElementById("address").value;
+			if (geocoder)
+			{
+				geocoder.geocoder({'address': address}, function(results, status)
+				{
+					if (status == google.maps.GeocoderStatus.OK)
+					{
+						map.setCenter(results[0].geometry.location);
+						var marker = new google.maps.Marker({map:map,position:results[0].geometry.location});
+					}
+					else
+					{
+						alert("檢視地圖失敗，原因：" + status);
+					}
+				});
+			}
+		}
+		</script>
+	</head>
+	<body onload="initialize()">
+		<input type="hidden" id="address" value="<?php echo $_POST["bookstore"]; ?>">
+		<div id="map_canvas" style="width:500px; height:500px;"></div>
+		<a href="map.php">上一頁</a>
+	</body>
+</html>
